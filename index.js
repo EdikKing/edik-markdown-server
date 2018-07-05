2c58c04336e9f3a7ff234daa4972d04d4262282e
Edik <wfj@edik.cn>
2018-07-05 21:31:40 +0800

    添加cors

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
 const Koa = require('koa')
 const app = new Koa()
+var router = require('koa-router')()
+var cors = require('koa2-cors');
 const markdown = require('./lib/markdown/index')
 
-app.use( async ( ctx ) => {
-    //当请求时GET请求时，显示表单让用户填写
-    if(ctx.url==='/' && ctx.method === 'GET'){
-        let html =`
-            <h1>Edik Markdown 解析接口</h1>
-        `;
-        ctx.body =html;
-        //当请求时POST请求时
-    }else if(ctx.url==='/' && ctx.method === 'POST'){
-        let pastData=await parsePostData(ctx);
-        let aa = parseQueryStr(pastData);
-        ctx.body=markdown().render(aa.html).html;
-    }else{
-        //其它请求显示404页面
-        ctx.body='<h1>404!</h1>';
-    }
-    // ctx.body = markdown().render(s).html
-})
+app.use(cors({
+    origin: function (ctx) {
+        if (ctx.url === '/test') {
+            return '*';
+        }
+        return '*';
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}));
 
-function parsePostData(ctx){
-    return new Promise((resolve,reject)=>{
-        try{
-            let postdata="";
-            ctx.req.on('data',(data)=>{
+router.post('/', async function (ctx) {
+    let pastData = await parsePostData(ctx);
+    let aa = parseQueryStr(pastData);
+
+    ctx.body = markdown().render(aa.html).html;
+});
+
+function parsePostData(ctx) {
+    return new Promise((resolve, reject) => {
+        try {
+            let postdata = "";
+            ctx.req.on('data', (data) => {
                 postdata += data
             })
-            ctx.req.addListener("end",function(){
+            ctx.req.addListener("end", function () {
 
                 resolve(postdata);
             })
-        }catch(error){
+        } catch (error) {
             reject(error);
         }
     });
 }
 
-function parseQueryStr(queryStr){
-    let queryData={};
+function parseQueryStr(queryStr) {
+    let queryData = {};
     let queryStrList = queryStr.split('&');
-    for( let [index,queryStr] of queryStrList.entries() ){
+    for (let [index, queryStr] of queryStrList.entries()) {
         let itemList = queryStr.split('=');
         queryData[itemList[0]] = decodeURIComponent(itemList[1]);
     }
     return queryData
 }
 
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(9016)
-console.log('[demo] start-quick is starting at port 3000')
+console.log('[demo] start-quick is starting at port 9016')
